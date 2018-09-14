d6282f34205ce37841543031fd369d33ba6174c1
Jaytrin <jaytrin.apinchapong@gmail.com>
2018-09-13 19:18:22 -0700

    feature set 1 complete

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react';
+
+export default () => {
+    return <h1>My first React functional component</h1>
+}
+
+// function App(){
+// return <h1>My first React functional component</h1>
+// }
+
+// export default App;
 
 
