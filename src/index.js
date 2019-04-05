dad86945d57b5fc8e8700ace683935059a01bb7f
Jaytrin <jaytrin.apinchapong@gmail.com>
2019-04-04 18:19:42 -0700

    completed feature set 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function luckyNumber(){
+    return Math.floor(1001 * Math.random());
+}
+
+function Greeting(props){
+    return (
+        <div className="container">
+            <h1>{props.name}</h1>
+            <h2 className="text-muted">{props.luckyNumber}</h2>
+        </div>
+    )
+}
+
+const userObject = {
+    name: "Welcome Jaytrin",
+    luckyNumber: luckyNumber()
+}
+
 ReactDOM.render(
-    <h1 className="container text-center">React Prototypes!</h1>,
+    <Greeting name={userObject.name} luckyNumber={userObject.luckyNumber} />,
     document.getElementById('root')
 );
+
